31c2e0dcb5dea58bf8b84b2af10cf5a8c4be4d2a
justyshen <speedoooshen@gmail.com>
2017-06-19 15:33:10 +0800

    加入Session機制

--- WebAPI_Train/App_Start/WebApiConfig.cs
+++ WebAPI_Train/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web.Http;
+using System.Web.SessionState;
+using System.Web.Http.WebHost;
+using System.Web.Routing;
+using System.Web;
 
 namespace WebAPI_Train
 {
@@ -12,13 +15,31 @@ namespace WebAPI_Train
             // Web API 設定和服務
 
             // Web API 路由
-            config.MapHttpAttributeRoutes();
+            config.MapHttpAttributeRoutes(); 
 
-            config.Routes.MapHttpRoute(
+            RouteTable.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
+            ).RouteHandler = new SessionRouteHandler();
+ 
+
+        }
+    }
+
+    public class SessionControllerHandler : HttpControllerHandler, IRequiresSessionState
+    {
+        public SessionControllerHandler(RouteData routeData)
+            : base(routeData)
+        { }
+    }
+
+
+    public class SessionRouteHandler : IRouteHandler
+    {
+        IHttpHandler IRouteHandler.GetHttpHandler(RequestContext requestContext)
+        {
+            return new SessionControllerHandler(requestContext.RouteData);
         }
     }
 }
