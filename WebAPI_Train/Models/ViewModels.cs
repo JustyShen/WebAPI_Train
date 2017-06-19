b3096e6d6e42378ffad7b1cfa272768f0315df69
justyshen <speedoooshen@gmail.com>
2017-06-19 15:24:54 +0800

    建立ViewModel，把所有可能的資料格式定義好

---
+++ WebAPI_Train/Models/ViewModels.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_Train.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string userId { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+
+    public class Login
+    { 
+        public string uid { get; set; } 
+        public string pwd { get; set; } 
+    }
+
+    public class Group
+    {
+        public int id { get; set; }
+        public string groupName { get; set; }
+        public List<User> gUsers { get; set; }
+    }
+
+    public class Chat
+    {
+        public int id { get; set; }
+        public int groupId { get; set; }
+        public User cUser { get; set; }
+        public string message { get; set; } 
+        public DateTime creatDate { get; set; }
+    } 
+
+}
